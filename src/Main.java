78837259ae42eb64544cea5aff10f46c2bfcc734
Burdusa Daniel <danielburdusa26@gmail.com>
2023-07-20 16:46:36 +0200

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Prodotto product = new Prodotto("cappello","bello" ,5,0.22 );
+        System.out.println("il nome del prodotto è "+product.getNome());
+        System.out.println("descrizone:"+product.getDescrizione());
+        System.out.println("prezzo base:"+product.getPrezzo());
+        double prezzo = product.prezzoIva(product.getPrezzo());
+        System.out.println("il prezzo totale del prodotto è :"+ prezzo);
+        System.out.println("il codice è :"+product.getCodice());
+        System.out.println("nome completo :"+product.toString());
+        System.out.println("iva"+product.getIva());
+        product.setNome("pippo");
+        product.setDescrizione("pippotto");
+        product.setPrezzo(6);
+        product.setIva(0.33);
+        System.out.println("name+:"+product.getNome());
+        System.out.println("descrizione+"+product.getDescrizione());
+        System.out.println("prezzo+"+product.getPrezzo());
+        System.out.println("iva+"+product.getIva());
+        System.out.println("full prezzo 2 "+product.prezzoIva(product.getPrezzo()));
+        System.out.println("nome+"+product.toString());
+    }
 }
--- src/Prodotto.java
+++ src/Prodotto.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class Prodotto {
+import java.util.Random;
+
+public class Prodotto {
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+    private double iva;
+
+    public Prodotto(String nome , String descrizione ,double prezzo,double iva ){
+        this.codice=codiceProdotto();
+        this.nome=nome;
+        this.descrizione=descrizione;
+        this.prezzo=prezzo;
+        this.iva=iva;
+    }
+
+    public int getCodice() {
+        return codice;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(int prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+    public int codiceProdotto(){
+        Random random=new Random();
+        int code=random.nextInt(10000000,100000000);
+        return code;
+    }
+
+    public double prezzoIva(double prezzo){
+       return prezzo+(prezzo*iva);
+    }
+
+    @Override
+    public String toString() {
+        return nome +"-"+ codice;
+    }
 }
